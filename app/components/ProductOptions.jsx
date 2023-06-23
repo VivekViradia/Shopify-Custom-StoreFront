0ab7bb6219c82af57d3750955e5f67ec4826633a
Vivek Viradia <vivekviradia123@gmail.com>
2023-06-23 18:28:52 +0530

    Variant Issue Fixed

--- app/components/ProductOptions.jsx
+++ app/components/ProductOptions.jsx
@@ -6,40 +6,38 @@ import {
 } from '@remix-run/react';
 
 export default function ProductOptions({options}) {
-  // pathname & search will be used to build option URls
   const {pathname, search} = useLocation();
   const [currentSearchParams] = useSearchParams();
   const navigation = useNavigation();
 
+  // Update the in-flight request data from the 'navigation' (if available)
+  // to create an optimistic UI that selects a link before the request completes
   const searchParams = navigation.location
     ? new URLSearchParams(navigation.location.search)
     : currentSearchParams;
 
   return (
     <div className="grid gap-4 mb-6">
-      {/* Each option will show a label & option value <Links> */}
       {options.map((option) => {
         if (!option.values.length) {
           return;
         }
 
-        //   get the currently selected option value
+        // get the currently selected option value
         const currentOptionVal = searchParams.get(option.name);
         return (
           <div
             key={option.name}
             className="flex flex-col flex-wrap mb-4 gap-y-2 last:mb-0"
           >
-            <h3 className="whitespace-pre-wrap max-w-prose font-bold text-lead min-w[4rem]">
+            <h3 className="whitespace-pre-wrap max-w-prose font-bold text-lead min-w-[4rem]">
               {option.name}
             </h3>
 
             <div className="flex flex-wrap items-baseline gap-4">
-              {option.value.map((value) => {
-                // Build a URLSearchParam object from the current search string
-                const linkParams = new URLSearchParams(searchP);
+              {option.values.map((value) => {
+                const linkParams = new URLSearchParams(searchParams);
                 const isSelected = currentOptionVal === value;
-                //   Set the options name & value,overwriting any existing values
                 linkParams.set(option.name, value);
                 return (
                   <Link
@@ -48,7 +46,7 @@ export default function ProductOptions({options}) {
                     preventScrollReset
                     replace
                     className={`leading-none py-1 border-b-[1.5px] cursor-pointer transition-all duration-200 ${
-                      isSelected ? 'broder-gray-500' : 'broder-neutral-50'
+                      isSelected ? 'border-gray-500' : 'border-neutral-50'
                     }`}
                   >
                     {value}
