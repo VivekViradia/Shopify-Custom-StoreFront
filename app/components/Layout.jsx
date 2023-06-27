91d839f19fab835c7678d9443e2e8d0b07bae6ab
Vivek Viradia <vivekviradia123@gmail.com>
2023-06-27 17:54:32 +0530

    Render Cart Components in the Drawer

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -1,6 +1,45 @@
 import {Suspense, useEffect} from 'react';
 import {Drawer, useDrawer} from '~/components/Drawer';
 import {Await, useMatches} from '@remix-run/react';
+import {CartLineItems, CartActions, CartSummary} from './Cart';
+
+function CartDrawer({cart, close}) {
+  return (
+    <Suspense>
+      <Await resolve={cart}>
+        {(data) => (
+          <>
+            {data?.totalQuantity > 0 ? (
+              <>
+                <div className="flex-1 overflow-y-auto">
+                  <div className="flex flex-col space-y-7 justify-between items-center md:py-8 md:px-12 px-4 py-6">
+                    <CartLineItems linesObj={data.lines} />
+                  </div>
+                </div>
+                <div className="w-full md:px-12 px-4 py-6 space-y-6 border border-1 broder-gray-00">
+                  <CartSummary cost={data.cost} />
+                  <CartActions checkoutUrl={data.checkoutUrl} />
+                </div>
+              </>
+            ) : (
+              <div className="flex flex-col space-y-7 justify-center items-center md:py-8 md:px-12 px-4 py-6 h-screen">
+                <h2 className="whitespace-pre-wrap max-w-prose font-bold text-4xl">
+                  Your Cart is Empty
+                </h2>
+                <button
+                  onClick={close}
+                  className="inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none bg-black text-white w-full"
+                >
+                  Continue Shopping
+                </button>
+              </div>
+            )}
+          </>
+        )}
+      </Await>
+    </Suspense>
+  );
+}
 
 function CartHeader({cart, openDrawer}) {
   return (
@@ -60,7 +99,7 @@ export function Layout({children, title}) {
         {children}
       </main>
       <Drawer open={isOpen} onClose={closeDrawer}>
-        <h2>TODO Cart Data</h2>
+        <CartDrawer cart={cart} close={closeDrawer} />
       </Drawer>
     </div>
   );
